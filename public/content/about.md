9812e7393941392736a59b49d12aa98d0c15644d
Janio Isacura <janioisacura@gmail.com>
2025-04-05 14:49:20 -0300

    Create static sections from markdown

---
+++ public/content/about.md
@@ -0,0 +1,14 @@
+# ¡Hola, soy Janio! 👋
+
+Este es un ejemplo de portfolio usando **Markdown**.
+
+## Cosas que sé hacer:
+- Angular
+- React
+- FastAPI
+- Docker
+- Hablar con AI en lenguaje humano
+
+```python
+# Ejemplo de código Python
+print("Hola desde Markdown")
---
+++ src/app/components/MarkdownViewer.tsx
@@ -0,0 +1,47 @@
+'use client'
+
+import { useEffect, useState } from 'react'
+import ReactMarkdown from 'react-markdown'
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
+import { oneDark } from 'react-syntax-highlighter/dist/esm/styles/prism'
+
+export default function MarkdownViewer() {
+  const [markdown, setMarkdown] = useState('Cargando...')
+  //const urlLocal = '/content/about.md'
+  const urlRemote = 'https://raw.githubusercontent.com/Yanioconjota/yanioconjota/main/README.md'
+
+  useEffect(() => {
+    fetch(urlRemote)
+      .then(res => res.text())
+      .then(setMarkdown)
+  }, [])
+
+  return (
+    <div className="prose max-w-3xl mx-auto p-6">
+      <ReactMarkdown
+        components={{
+          // eslint-disable-next-line @typescript-eslint/no-explicit-any
+          code({ inline, className, children, ...props }: any) {
+            const match = /language-(\w+)/.exec(className || '')
+            return !inline && match ? (
+              <SyntaxHighlighter
+                style={oneDark}
+                language={match[1]}
+                PreTag="div"
+                {...props}
+              >
+                {String(children).replace(/\n$/, '')}
+              </SyntaxHighlighter>
+            ) : (
+              <code className={className} {...props}>
+                {children}
+              </code>
+            )
+          }
+        }}
+      >
+        {markdown}
+      </ReactMarkdown>
+    </div>
+  )
+}
